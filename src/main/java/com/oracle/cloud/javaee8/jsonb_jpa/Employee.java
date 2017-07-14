7c6a6d693670209ef1cbed79a31fea01ddd979cc
Abhishek <>
2017-07-14 12:02:33 +0530

    clean

--- src/main/java/com/oracle/cloud/javaee8/jsonb_jpa/Employee.java
+++ src/main/java/com/oracle/cloud/javaee8/jsonb_jpa/Employee.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.oracle.cloud.javaee8.jsonb_jpa;
 
 import javax.json.bind.annotation.JsonbProperty;
@@ -13,10 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- *
- * @author agupgupt
- */
 @Entity
 @Table(name = "Employees")
 @JsonbPropertyOrder(PropertyOrderStrategy.REVERSE)
--- src/main/java/com/oracle/cloud/javaee8/jsonb_jpa/JAXRSConfig.java
+++ src/main/java/com/oracle/cloud/javaee8/jsonb_jpa/JAXRSConfig.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.oracle.cloud.javaee8.jsonb_jpa;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- *
- * @author agupgupt
- */
 @ApplicationPath("")
 public class JAXRSConfig extends Application{
     
